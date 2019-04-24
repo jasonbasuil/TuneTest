8bb59ab98f375bab8a090d54aa17801f9772ff7e
jasonbasuil <basuilj@gmail.com>
2019-04-24 13:03:29 -0700

    Finished with login button and redirect.

---
+++ src/containers/LoginView/Login.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <a href="http://localhost:3000/api/v1/login">
+          Sign in with Spotify
+        </a>
+      </div>
+    )
+  }
+}
+
+export default Login
